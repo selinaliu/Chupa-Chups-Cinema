9c5725229d8de71b0d9185296fca5236d01a8588
selinaliu_ <selinaliu_@users.noreply.github.com>
2020-10-19 14:59:57 +0800

    add slideshow for poster

---
+++ poster.js
@@ -0,0 +1,15 @@
+function chngimg(index) {
+
+   switch(index){
+       case 1:
+            document.getElementById("poster").src  = "mulan.webp";
+            break;
+        case 2:
+            document.getElementById("poster").src  = "tenetP.jpg";
+            break;
+        case 3:
+            document.getElementById("poster").src  = "vanguardP.jpg";
+            break;
+   }
+
+}
